424d2d2d58c3b5433c987f3d6fdf1fc0d9bbcf80
AndyTechnologies <gardyfon2403@gmail.com>
2025-07-17 13:11:27 -0400

    Agregando configuraciones para Astro y ContentsCollections.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: true,
+  site: 'https://www.github.com/',
+
+  i18n: {
+      locales: ["es", "en"],
+      defaultLocale: "es",
+      fallback: {
+          en: "es"
+      },
+      routing: {
+          prefixDefaultLocale: false,
+          fallbackType: "rewrite"
+      }
+  },
+
+  integrations: [mdx()]
+});
